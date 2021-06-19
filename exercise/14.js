305de396dc8d07941b8eade9910f5bee7796a6be
yelqgd <yelqgd@gmail.com>
2021-06-19 12:58:16 +0800

    add 14.js

---
+++ exercise/14.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+const longestCommonPrefix = function (strs) {
+  if (strs.length === 0) {
+    return ''
+  }
+
+  if (strs.length === 1) {
+    return strs[0]
+  }
+
+  let min = 0
+  let max = 0
+
+  for (let i = 1; i < strs.length; i++) {
+    if (strs[min] > strs[i]) {
+      min = i
+    }
+    if (strs[max] < strs[i]) {
+      max = i
+    }
+  }
+
+  for (let j = 0; j < strs[min].length; j++) {
+    if (strs[min].charAt(j) !== strs[max].charAt(j)) {
+      return strs[min].substring(0, j)
+    }
+  }
+
+  return strs[min]
+}
